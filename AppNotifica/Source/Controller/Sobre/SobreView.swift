3693dcb6bcf427ae2061405b2669a1432508e430
Eduardo Moreira <eduardo63008@estudante.ifb.edu.br>
2024-08-19 09:10:58 -0300

    start

---
+++ AppNotifica/Source/Controller/Sobre/SobreView.swift
@@ -0,0 +1,24 @@
+//
+//  SobreView.swift
+//  AppNotifica
+//
+//  Created by Dario Pintor on 25/10/22.
+//
+
+import Foundation
+import UIKit
+
+class SobreView: ViewDefault {
+    //MARK: - Closures
+    
+    //MARK: - Proports
+    
+    //MARK: - Inits
+    
+    
+    
+   override  func setupVisualElements() {
+
+    }
+    
+}
